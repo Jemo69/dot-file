346c810b2bdd0bc4fa3a5e2f5d669f7ca5f076e4
Jemo69 <jemolife69@gmail.com>
2025-11-04 18:00:41 -0500

    custom snippet

--- .config/nvim/lua/custom_snippets.lua
+++ .config/nvim/lua/custom_snippets.lua
@@ -5,12 +5,29 @@ local t = ls.text_node
 local i = ls.insert_node
 
 ls.add_snippets("python", {
-  s("dmodel", {
-    t("class "),
-    i(1, "ModelName"),
-    t("(models.Model):"),
-    t({"", "\t"}),
-    i(2, "pass"),
-    t({"", ""})
-  })
+    s("dmodel", {
+        t("class "),
+        i(1, "ModelName"),
+        t("(models.Model):"),
+        t({ "", "\t" }),
+        i(2, "pass"),
+        t({ "", "" })
+    })
+})
+ls.add_snippets("typescriptreact", {
+    s("tanstack-component", {
+        t("const"),
+        i(1, "  componentName"),
+        i(1, "= () => {"),
+        t({ "", "\t" }),
+        i(2, "return"),
+        t({ "", "\t" }),
+        i(3, "<div>"),
+        t({ "", "\t" }),
+        i(4, "</div>"),
+        t({ "", "\t" }),
+        i(5, "}"),
+        t({ "", "" })
+
+    })
 })
